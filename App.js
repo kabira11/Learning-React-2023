d6509f8cedf76bc261d7ecee8a8d5764849548dd
Pankaj Bhardwaj <pankajbhj@gmail.com>
2023-09-05 22:46:23 +0530

    Running react with CDN

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading1 = React.createElement(
+    "h1",
+    {id : 'heading'},
+    "Hello World From React!")
+
+const root1 = ReactDOM.createRoot(document.getElementById("root"))
+
+root1.render(heading1);
